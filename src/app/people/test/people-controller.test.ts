5c3ec9ce660faaf21ef1b69a86f507e83145d9a9
Savioalbuquerque789 <savioribeiro9855@gmail.com>
2025-06-20 00:49:50 -0300

    refactor: Mudança do nome das pastas Handler para Controller

---
+++ src/app/people/test/people-controller.test.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from "vitest";
+import { PeopleController } from "../people-controller";
+
+describe("People Controller Test",async () => {
+    const peopleController = new PeopleController()
+    it("Insert",async () => {
+
+    })
+     
+})
